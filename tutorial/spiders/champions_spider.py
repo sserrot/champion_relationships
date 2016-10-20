03a0f89e28d203a0b9b7602199bfb57235eaff85
Santiago Torres <santiago6695@gmail.com>
2016-10-20 02:14:05 -0400

    JSON object encoded
    Parser extracts all champion names
    TODO- separate by Rival/Friend

--- tutorial/spiders/champions_spider.py
+++ tutorial/spiders/champions_spider.py
@@ -19,19 +19,23 @@ class ChampionsSpider(scrapy.Spider):
     #
     # full_url.append('http://gameinfo.na.leagueoflegends.com/en/game-info/champions/monkeyking/')  #  rito why?
 
-    test_url = ['http://gameinfo.na.leagueoflegends.com/en/game-info/champions/monkeyking/']
+    test_url = ['http://gameinfo.na.leagueoflegends.com/en/game-info/champions/darius/']
 
     start_urls = test_url
 
     def parse(self, response):
         # use lxml to get decent HTML parsing speed
         soup = BeautifulSoup(response.text, 'lxml')
-        yield {
-            "url": response.url,
-            "title": soup.div.string
-        }
 
-        # if str(quote.css('header.header::text').extract()) == 'Rivals':
+        # This does not differentiate between Friends and Rivals
+
+        # Goal - json object with friends_champion_names and then rivals_champion_names
+
+        k = [' ' + name['data-rg-id'] for name in soup.find_all('div', class_="champ-name")]
+
+        yield {
+            "champion_names": k
+        }
 
 
         # # save in html
